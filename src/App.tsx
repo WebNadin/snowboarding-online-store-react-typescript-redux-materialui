2890336fea9fffa133f8ccc1f190c3794cd8afef
Nadiia <nenadinweb@gmail.com>
2022-03-31 20:51:49 +0300

    header was added

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 import FormComment from "./components/FormComment";
 import Comments from "./components/Comments";
 import FetchedComments from "./components/FetchedComments";
+import Header from './components/Header';
 
 const CustomInput = (props: React.HTMLProps<HTMLInputElement>) => {
   const [value, setValue] = useState<number>(100);
@@ -34,63 +35,66 @@ async function getPerson ( id = 1): Promise<Person> {
   return response.json();
 }
 
-const CardPerson = () => {
-  const [person, setPerson] = useState<Person | null>(null);
-
-  const ref = useRef<HTMLDivElement>(null);
-
-  // useEffect(() => {
-  //     setInterval(() => {
-  //         value.current += 1;
-  //         console.log(value);
-  //     }, 1000)
-  // })
-
-  useEffect( () => {
-    getPerson().then(data => setPerson(data))
-  }, [])
-
-  return(
-    <p ref={ref}>
-      {person?.name}
-    </p>
-  )
-}
-
-const Title = styled.h1`
-  color: brown;
-  font-size: 2rem;
-`
+// const CardPerson = () => {
+//   const [person, setPerson] = useState<Person | null>(null);
+//
+//   const ref = useRef<HTMLDivElement>(null);
+//
+//   // useEffect(() => {
+//   //     setInterval(() => {
+//   //         value.current += 1;
+//   //         console.log(value);
+//   //     }, 1000)
+//   // })
+//
+//   useEffect( () => {
+//     getPerson().then(data => setPerson(data))
+//   }, [])
+//
+//   return(
+//     <p ref={ref}>
+//       {person?.name}
+//     </p>
+//   )
+// }
 
 export default function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <FormComment />
-        </div>
-      </div>
-      <div className="row">
-        <div className="col">
-          <h2>Синхронные комментарии</h2>
-          <Comments />
-        </div>
-        <div className="col">
-          <h2>Асинхронные комментарии</h2>
-          <FetchedComments />
-        </div>
-      </div>
-      <Title>
-        <a href="/">Test Title</a>
-      </Title>
-      <Typography variant="subtitle1" gutterBottom component="div" color="primary.dark">
-        subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-        blanditiis tenetur
-      </Typography>
-      <CardPerson/>
-      <CustomInput  type={"text"}>
-      </CustomInput>
-    </div>
+    <>
+      <Header />
+    </>
   );
 };
+
+// export default function App() {
+//   return (
+//     <div className="container">
+//       <div className="row">
+//         <div className="col">
+//           <FormComment />
+//         </div>
+//       </div>
+//       <div className="row">
+//         <div className="col">
+//           <h2>Синхронные комментарии</h2>
+//           <Comments />
+//         </div>
+//         <div className="col">
+//           <h2>Асинхронные комментарии</h2>
+//           <FetchedComments />
+//         </div>
+//       </div>
+//       <Title>
+//         <a href="/">Test Title</a>
+//       </Title>
+//       <Typography variant="subtitle1" gutterBottom component="div" color="primary.dark">
+//         subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+//         blanditiis tenetur
+//       </Typography>
+//       <CardPerson/>
+//       <CustomInput  type={"text"}>
+//       </CustomInput>
+//     </div>
+//   );
+// };
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,150 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import { Menu as MenuIcon } from "@material-ui/icons";
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import Badge from '@mui/material/Badge';
+import { ShoppingCart as ShoppingCartIcon } from '@material-ui/icons';
+
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+const Header = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Typography
+          variant="h6"
+          noWrap
+          component="div"
+          sx={{ display: 'flex' }}
+        >
+          SNOWBOARDING
+        </Typography>
+        <Box sx={{ flexGrow: 0 }}>
+          <Tooltip title="Open settings">
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: '45px' }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {settings.map((setting) => (
+              <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                <Typography textAlign="center">{setting}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+      </Container>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+          >
+            LOGO
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={4} color="error">
+                <ShoppingCartIcon />
+              </Badge>
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default Header;
