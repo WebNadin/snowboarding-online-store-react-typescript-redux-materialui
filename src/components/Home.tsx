7e4b96f28801ed944d92199bc3ee40d2cbe6c9e6
Nadiia <nenadinweb@gmail.com>
2022-04-28 16:01:54 +0300

    products component grid was added

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,11 @@
 import Box from "@mui/material/Box";
 import {myTheme} from "../styles/myTheme";
+import Products from "./Products";
 
 const Home = () => {
   return (
-    <Box sx={{bgcolor: myTheme.light.background.primary, height: "100%"}}>
-      test commit
+    <Box sx={{bgcolor: myTheme.light.background.secondary, height: "100%"}}>
+      <Products />
     </Box>
   )
 }
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,8 +1,65 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import {myTheme} from "../styles/myTheme";
+
+const Tools = () => {
+  return (
+    <Box sx={{
+      display: 'flex',
+      justifyContent: 'flex-end',
+    }}>
+      <Box sx={{
+        bgcolor: myTheme.dark.text.secondary
+      }}>
+        iconbutton + drawer showing by default and by click
+      </Box>
+      <Box sx={{
+        ml: {
+          xs: 0,
+          sm: 2,
+        },
+        bgcolor: myTheme.dark.text.secondary
+      }}>dropdown with several items</Box>
+    </Box>
+  )
+}
+
+const ProductsList = () => {
+  return (
+    <Box sx={{
+      bgcolor: '#b9f1d4de',
+      flexGrow: 1,
+    }}>
+      products list
+    </Box>
+  )
+}
+const Filters = () => {
+  return (
+    <Box sx={{
+      bgcolor: myTheme.dark.background.secondary,
+      color: myTheme.dark.text.contrast,
+      width: '16em',
+    }}>
+      2 accordions (type and brand filters)with checkboxes in it
+    </Box>
+  )
+}
+
 const Products = () => {
   return (
-    <div>
-      Products component
-    </div>
+    <Box>
+      <Container maxWidth="xl">
+        <Tools />
+        <Box sx={{
+          display: 'flex',
+        }}>
+          <Filters />
+          <ProductsList />
+        </Box>
+      </Container>
+    </Box>
   )
 }
 
