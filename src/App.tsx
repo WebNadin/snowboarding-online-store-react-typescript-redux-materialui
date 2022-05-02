586d83ed9d4d09c62475b02f480f50d9fd03d6e8
Nadiia <nenadinweb@gmail.com>
2022-05-02 20:52:23 +0300

    router changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,16 @@ import './App.css';
 import Header from './components/Header';
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import {BrowserRouter} from "react-router-dom";
 import { Box } from '@mui/system';
 
 
 export default function App() {
   return (
-    <BrowserRouter>
       <Box sx={{display: "flex", flexDirection: "column", height: "100%"}}>
         <Header />
         <Main />
         <Footer />
       </Box>
-    </BrowserRouter>
   );
 };
 
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,7 @@
 import {
   Routes,
   Route,
+  BrowserRouter
 } from "react-router-dom";
 import Home from "./Home";
 import Products from "./Products";
@@ -11,12 +12,14 @@ import Box from "@mui/material/Box";
 const Main = () => {
  return (
   <Box sx={{flex: "1 0 auto"}}>
+    <BrowserRouter>
        <Routes>
            <Route index element={<Home />} />
            <Route path="products" element={<Products />} />
            <Route path="about" element={<About />} />
            <Route path="contact" element={<Contact />} />
        </Routes>
+    </BrowserRouter>
   </Box>
  )
 }
